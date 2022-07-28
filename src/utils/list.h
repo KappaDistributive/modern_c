384f819776467d94e1666ddff84d362e305dfe9f
Stefan Mesken <stefan.mesken@deepl.com>
2022-07-28 18:16:01 +0200

    linting

--- src/utils/list.h
+++ src/utils/list.h
@@ -2,27 +2,26 @@
 #include <stdlib.h>
 
 struct SNode {
-  void* data;
-  struct SNode* next;
+  void *data;
+  struct SNode *next;
 };
-
 
 typedef struct SNode SNode;
 
-SNode* SList_create(void* data, size_t data_size);
+SNode *SList_create(void *data, size_t data_size);
 
-void SList_free(SNode* list);
+void SList_free(SNode *list);
 
-size_t SList_length(SNode const* const list);
+size_t SList_length(SNode const *const list);
 
-SNode* SList_append(SNode* const list, void* data, size_t data_size);
+SNode *SList_append(SNode *const list, void *data, size_t data_size);
 
-SNode* SList_prepend(SNode* const list, void* data, size_t data_size);
+SNode *SList_prepend(SNode *const list, void *data, size_t data_size);
 
-SNode* SList_get_item(SNode* list, size_t position);
+SNode *SList_get_item(SNode *list, size_t position);
 
-void* SList_get_data(SNode* list, size_t position);
+void *SList_get_data(SNode *list, size_t position);
 
-SNode* SList_pop_left(SNode* list);
+SNode *SList_pop_left(SNode *list);
 
-SNode* SList_pop_right(SNode* list);
+SNode *SList_pop_right(SNode *list);
